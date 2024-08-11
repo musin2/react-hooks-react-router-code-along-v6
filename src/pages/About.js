20399a7d7ef94f93c6a901ffbb59cabfb9920cb3
musin2 <muhsindaud75@gmail.com>
2024-08-11 18:09:41 +0300

    adds client-side routing

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import NavBar from "../components/NavBar";
+
+// About.js
+function About() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>This is my about component!</h1>
+      </main>
+    </>
+  );
+}
+
+export default About;
